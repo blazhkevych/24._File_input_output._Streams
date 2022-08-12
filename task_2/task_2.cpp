ed66baebd78f4ab9047b0a814723765e7abdadab
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-08-12 15:43:12 +0300

    правки

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -1,8 +1,11 @@
 ﻿/*
- 2. Написать программу, которая разбивает файл любого
+############################################ ЗАДАНИЕ ###########################################
+			Файловый ввод - вывод с применением потоков
+	2. Написать программу, которая разбивает файл любого
 формата на кусочки указанного размера, а затем
 восстанавливаем файл из этих кусков.
- */
+################################################################################################
+*/
 
 #include <iostream>
 #include <fstream>
@@ -11,54 +14,72 @@ using namespace std;
 
 int main()
 {
-    SetConsoleCP(1251);
-    /*
-    Задает кодовую страницу ввода, используемую консолью, связанной с вызывающим процессом.
-    Консоль использует страницу входного кода для преобразования ввода с клавиатуры
-    в соответствующее символьное значение.
-    */
-    SetConsoleOutputCP(1251);
-    /*
-    Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом.
-    Консоль использует свою кодовую страницу вывода для преобразования символьных значений,
-    записанных различными функциями вывода, в изображения, отображаемые в окне консоли.
-    */
+	SetConsoleCP(1251);
+	/*
+	Задает кодовую страницу ввода, используемую консолью, связанной с вызывающим процессом.
+	Консоль использует страницу входного кода для преобразования ввода с клавиатуры
+	в соответствующее символьное значение.
+	*/
+	SetConsoleOutputCP(1251);
+	/*
+	Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом.
+	Консоль использует свою кодовую страницу вывода для преобразования символьных значений,
+	записанных различными функциями вывода, в изображения, отображаемые в окне консоли.
+	*/
 
-    cout << "Введите путь: "; // 1.jpg
-    string str;
-    cin >> str;
+	cout << "Введите путь к исходному файлу: "; // 1.jpg
+	string sourcePath{};
+	cin >> sourcePath;
 
-    ifstream in(str, ios::binary);
-    if (!in) // если открытие файла завершилось неудачей - выходим
-    {
-        cout << "Error!\n";
-        return;
-    }
+	cout << "Введите размер 1 кусочка в байтах: ";
+	long long onePieceSizeInBytes{};
+	cin >> onePieceSizeInBytes;
 
-    in.seekg(0, ios::end);
-    int const count = in.tellg();
-    in.seekg(0, ios::beg);
-    cout << count << endl;
+	ifstream readSourceFile(sourcePath, ios::binary | ios::in); // открывается  файл на чтение в бинарном режиме : при этом файл должен существовать
+	if (!readSourceFile) // если открытие файла завершилось неудачей - выходим
+	{
+		cout << "Error!\n";
+		return 0;
+	}
 
-    char File[50];
-    char* buf = new char[size];
+	readSourceFile.seekg(0, ios::end); // смещение файлового указателя на 0 от конца
+	long long sourceFileSize = readSourceFile.tellg(); // размер исходного файла
+	readSourceFile.seekg(0, ios::beg); // смещение файлового указателя на 0 от начала
 
-    for (int number = 0; in; ++number) // цикл продолжается до тех пор, пока не наступит конец файла
-    {
-        sprintf(File, "f:\\File%d.txt", number);
-        ofstream out(File, ios::binary);
+	char* bufForDataPiece = new char[onePieceSizeInBytes] {}; // буфер на 1 кусочек
 
-        if (number == 0)
-        {
-            out.write(f, strlen(f) + 1);
-        }
+	int i{ 0 };
+	int countParts{};
+	while (!readSourceFile.eof())
+	{
+		char bufferFileName[50];
+		snprintf(bufferFileName, sizeof(bufferFileName), "file_%d.bin", i++);
+		// открывается  файл на запись в бинарном режиме : если файл не существует - он будет создан, если файл существует - он будет усечён до нулевой длины
+		ofstream writePieces(bufferFileName, ios::binary | ios::out);
+		readSourceFile.read(bufForDataPiece, onePieceSizeInBytes); // из входящего потока считывается в буфер блок данных (4096 байт)
+		writePieces.write(bufForDataPiece, readSourceFile.gcount()); // в выходной поток записывается считанный блок данных. При этом gcount возвращает реальное число считанных байт
+		writePieces.close();
+		countParts++;
+	}
+	readSourceFile.close();
+	delete[] bufForDataPiece;
 
-        in.read(buf, size);
-        out.write(buf, in.gcount());
-        out.close();
-    }
+	char* resultBuffer = new char[onePieceSizeInBytes + 1]; // память под результирующий файл в размере заданной части файла
 
-    delete[] buf;
-    in.close();
+	ofstream resultOfParts_write("1(copy).jpg", ios::binary | ios::out);
 
+	for (int j = 0; j < countParts; j++)
+	{
+		char partName[50]{};
+		snprintf(partName, sizeof(partName), "file_%d.bin", j++);
+		ifstream read(partName, ios::binary | ios::in); // открывается  файл на чтение в бинарном режиме : при этом файл должен существовать
+
+		read.read(resultBuffer, onePieceSizeInBytes);
+		resultOfParts_write.write(resultBuffer, read.gcount()); // в выходной поток записывается считанный блок данных. При этом gcount возвращает реальное число считанных байт
+
+		read.close();
+	}
+	resultOfParts_write.close();
+	cout << "Файл собран !" << endl;
+	delete[]resultBuffer;
 }
--- task_5/task_5.cpp
+++ task_5/task_5.cpp
@@ -1,20 +1,19 @@
-﻿// task_5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿/*
+############################################ ЗАДАНИЕ ###########################################
+			Файловый ввод - вывод с применением потоков	
+	5. Написать программу, которая считывает текст из файла и
+выводит в другой файл слова из этого текста, расположив
+слова в порядке убывания частоты их появления.При этом
+для каждого слова необходимо напечатать число его
+появлений в исходном файле
+################################################################################################
+подсчитать кол-во символов в файле(есть метод выдающий кол-во символов в файле) и выделить под него столько же памяти, а там уже подсчитывать
+по 3 примеру преподавателя можно делать
+*/
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
